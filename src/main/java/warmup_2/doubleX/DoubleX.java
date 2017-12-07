9774d61d8ca3a458df9034d489523d111989fe17
Darek Strojecki <dstroj@wp.pl>
2017-12-07 17:50:49 +0100

    Improvement of doubleXTest.

--- src/main/java/warmup_2/doubleX/DoubleX.java
+++ src/main/java/warmup_2/doubleX/DoubleX.java
@@ -1,9 +1,10 @@
 package warmup_2.doubleX;
 
 public class DoubleX {
+
     public static boolean doubleX(String str) {
         boolean result = false;
-        for (int i = 0; i < str.length()-2; i++) {
+        for (int i = 0; i < str.length()-1; i++) {
             if (str.charAt(i) == 'x' && str.charAt(i) == str.charAt(i+1)){
                 result = true;
                 break;
--- src/test/java/warmup_2/doubleX/doubleXtest.java
+++ src/test/java/warmup_2/doubleX/doubleXtest.java
@@ -1,5 +1,6 @@
 package warmup_2.doubleX;
 
+import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
@@ -8,32 +9,46 @@ public class doubleXtest {
 
     @Test
     public void testZero(){
-        assertEquals(false, DoubleX.doubleX("axax"));
+        assertEquals(true, DoubleX.doubleX("axxbb"));
     }
 
     @Test
     public void testOne(){
-        assertEquals(true, DoubleX.doubleX("axxa"));
+        assertEquals(true, DoubleX.doubleX("xxxxx"));
     }
 
     @Test
     public void testTwo(){
-        assertEquals(true, DoubleX.doubleX("xxxxxxxx"));
+        assertEquals(true, DoubleX.doubleX("xx"));
     }
+
     @Test
     public void testThree(){
-        assertEquals(false, DoubleX.doubleX("XxXxXXx"));
+        assertEquals(false, DoubleX.doubleX("xaxxx"));
     }
 
     @Test
     public void testFour(){
+        assertEquals(false, DoubleX.doubleX("axaxax"));
+    }
+
+    @Test
+    public void testFive(){
+        assertEquals(false, DoubleX.doubleX("aaaax"));
+    }
+
+    @Test
+    public void testSix(){
         assertEquals(false, DoubleX.doubleX(""));
     }
 
+    @Test
+    public void testSeven(){
+        assertEquals(false, DoubleX.doubleX("abc"));
+    }
 
-
-
-
-
-
+    @Test
+    public void testEight(){
+        assertEquals(false, DoubleX.doubleX("x"));
+    }
 }
