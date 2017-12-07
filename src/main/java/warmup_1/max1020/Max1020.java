3e5c4f5cfa46e722f68ade5a35cd265d2d18de1c
Darek Strojecki <dstroj@wp.pl>
2017-12-07 13:26:46 +0100

    Refactor Max1020

--- src/main/java/warmup_1/max1020/Max1020.java
+++ src/main/java/warmup_1/max1020/Max1020.java
@@ -3,11 +3,14 @@ package warmup_1.max1020;
 public class Max1020 {
 
     public static int max1020(int a, int b) {
-        if (isValueBettwen10And20(a) && isValueBettwen10And20(b)) {
+        boolean aInRange = isValueBettwen10And20(a);
+        boolean bInRange = isValueBettwen10And20(b);
+
+        if (aInRange && bInRange) {
             return getLarger(a, b);
-        } else if (isValueBettwen10And20(a) && !isValueBettwen10And20(b)) {
+        } else if (aInRange && !bInRange) {
             return a;
-        } else if (!isValueBettwen10And20(a) && isValueBettwen10And20(b)) {
+        } else if (!aInRange && bInRange) {
             return b;
         }
         return 0;
