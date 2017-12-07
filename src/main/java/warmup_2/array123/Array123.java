920b2f5a00e8172f2369bba6fad86cff13251ca7
Darek Strojecki <dstroj@wp.pl>
2017-12-07 18:25:16 +0100

    Add Array123 and Array123Test

---
+++ src/main/java/warmup_2/array123/Array123.java
@@ -0,0 +1,19 @@
+package warmup_2.array123;
+
+public class Array123 {
+
+    public static boolean array123(int[] nums) throws Exception {
+        if (nums.length < 3) {
+            throw new Exception("Not enough integers (min. 3)");
+        }
+        boolean result = false;
+        for (int i = 0; i < nums.length-2; i++) {
+            if (nums[i] == 1) {
+                if ((nums[i + 1] == 2) && (nums[i + 2] == 3)) {
+                    result = true;
+                }
+            }
+        }
+        return result;
+    }
+}
---
+++ src/test/java/warmup_2/array123/array123Test.java
@@ -0,0 +1,41 @@
+package warmup_2.array123;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static warmup_2.array123.Array123.array123;
+
+public class array123Test {
+
+    @Test(expected = Exception.class)
+    public void testZero() throws Exception {
+        int[] array = {1, 2};
+        array123(array);
+    }
+
+    @Test
+    public void testOne() throws Exception {
+        int[] array = {1, 1, 1};
+        assertEquals(false, array123(array));
+    }
+
+    @Test
+    public void testTwo() throws Exception {
+        int[] array = {1, 2, 3};
+        assertEquals(true, array123(array));
+    }
+
+    @Test
+    public void testThree() throws Exception {
+        int[] array = {0, 7, 10, 1, 2, 3};
+        assertEquals(true, array123(array));
+    }
+
+    @Test
+    public void testFour() throws Exception {
+        int[] array = {1, 1, 2, 4, 1};
+        assertEquals(false, array123(array));
+    }
+
+
+}
