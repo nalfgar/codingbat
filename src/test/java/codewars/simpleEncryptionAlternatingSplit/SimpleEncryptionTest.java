2a5252a8675b36d6b5ae59162ba540cebeb8e332
Darek Strojecki <dstroj@wp.pl>
2017-12-09 21:08:49 +0100

    small changes

---
+++ src/test/java/codewars/simpleEncryptionAlternatingSplit/SimpleEncryptionTest.java
@@ -0,0 +1,70 @@
+package codewars.simpleEncryptionAlternatingSplit;
+
+import org.junit.Ignore;
+import org.junit.Test;
+import org.junit.experimental.categories.Category;
+
+import static codewars.simpleEncryptionAlternatingSplit.SimpleEncryption.*;
+import static org.junit.Assert.assertEquals;
+
+public class SimpleEncryptionTest {
+
+    @Test
+    public void whenMethodEncryptRecieveNull(){
+        assertEquals(null, encrypt(null, -1));
+    }
+
+    @Test
+    public void whenMethodEncryptRecieve0(){
+        assertEquals("This is a test!", encrypt("This is a test!", 0));
+    }
+
+    @Test
+    public void whenMethodEncryptRecieve1(){
+        assertEquals("hsi  etTi sats!", encrypt("This is a test!", 1));
+    }
+
+    @Test
+    public void whenMethodEncryptRecieve2(){
+        assertEquals("s eT ashi tist!", encrypt("This is a test!", 2));
+    }
+
+    @Test
+    public void whenMethodEncryptRecieve3(){
+        assertEquals(" Tah itse sits!", encrypt("This is a test!", 3));
+    }
+
+    @Test
+    public void whenMethodEncryptRecieve4(){
+        assertEquals("This is a test!", encrypt("This is a test!", 4));
+    }
+
+    @Test
+    public void whenMethodEncryptRecieveLessTanZero(){
+        assertEquals("This is a test!", encrypt("This is a test!", -1));
+    }
+
+
+    @Test
+    public void whenMethodDecryptRecieveNull(){
+        assertEquals(null, decrypt(null, -1));
+    }
+
+    @Test
+    public void whenMethodDecryptRecieveZero(){
+        assertEquals("This is a test!", decrypt("This is a test!", 0));
+    }
+
+    @Test
+    public void whenMethodDecryptRecieve1(){
+        assertEquals("This is a test!", decrypt("hsi  etTi sats!", 1));
+    }
+    @Test
+    public void whenMethodDecryptRecieve2(){
+        assertEquals("This is a test!", decrypt("s eT ashi tist!", 2));
+    }
+
+
+
+
+}
