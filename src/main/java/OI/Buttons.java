ac083b1b6ff41163df4085f47f82993ad992c4f9
Darek Strojecki <dstroj@wp.pl>
2018-01-04 16:55:05 +0100

    Delete Main method from Buttons class.

--- src/main/java/OI/Buttons.java
+++ src/main/java/OI/Buttons.java
@@ -1,13 +1,6 @@
 package OI;
 
 public class Buttons {
-    public static void main(String[] args) {
-        int[] inputTable = {1, 1, 1, 8, 1, 1, 1, 8, 2, 7};
-        int[] output = buttons(7, 10, inputTable);
-        for (int i = 0; i < output.length; i++) {
-            System.out.print(output[i] + " ");
-        }
-    }
 
     public static int[] buttons(int buttonCounter, int pushCounter, int[] input) {
         int[] result = new int[buttonCounter];
