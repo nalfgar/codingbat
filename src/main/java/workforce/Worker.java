2e8b7534f657272ef99b1577b63755cf6cecba15
Darek Strojecki <dstroj@wp.pl>
2018-01-11 12:17:02 +0100

    Add Worker and WorkerTest classes.

---
+++ src/main/java/workforce/Worker.java
@@ -0,0 +1,74 @@
+package workforce;
+
+
+// TODO - use Lombok (constructor, getter , setter, toString)
+public class Worker {
+    private String name;
+    private String surname;
+    private double salary;
+    private char gender;
+    private int department;
+
+   public boolean doesHeWorkInDepartment(int departmentNumber) {
+        return departmentNumber == department;
+    }
+
+    public Worker(String name, String surname, double salary, char gender, int department) {
+        this.name = name;
+        this.surname = surname;
+        this.salary = salary;
+        this.gender = gender;
+        this.department = department;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public char getGender() {
+        return gender;
+    }
+
+    public void setGender(char gender) {
+        this.gender = gender;
+    }
+
+    public int getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(int department) {
+        this.department = department;
+    }
+
+    @Override
+    public String toString() {
+        return "Worker{" +
+                "name='" + name + '\'' +
+                ", surname='" + surname + '\'' +
+                ", salary=" + salary +
+                ", gender=" + gender +
+                ", department=" + department +
+                '}';
+    }
+}
---
+++ src/test/java/workforce/WorkerTest.java
@@ -0,0 +1,22 @@
+package workforce;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class WorkerTest {
+
+    @Test
+    public void testDoesHeWorkInDepartment1(){
+        Worker worker = new Worker("Darek", "Strojecki", 2500.0, 'M', 100);
+        assertFalse(worker.doesHeWorkInDepartment(10));
+    }
+
+    @Test
+    public void testDoesHeWorkInDepartment2(){
+        Worker worker = new Worker("Darek", "Strojecki", 2500.0, 'M', 10);
+        assertTrue(worker.doesHeWorkInDepartment(10));
+    }
+}
