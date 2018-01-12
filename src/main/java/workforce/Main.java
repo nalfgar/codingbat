2add21744be7b85fcd6856e13ae2f6aaad4f8a18
Darek Strojecki <dstroj@wp.pl>
2018-01-12 17:12:34 +0100

    Add methods to Workforce class: svaeBinary() and openBinary. OpenBunary() does not work correctly!!!

--- src/main/java/workforce/Main.java
+++ src/main/java/workforce/Main.java
@@ -3,10 +3,20 @@ package workforce;
 public class Main {
     public static void main(String[] args) {
         Workforce firm = new Workforce(5);
-        firm.addWorkerIneractive();
-        firm.addWorkerIneractive();
 
-        System.out.println(firm);
+        Worker worker1 = new Worker("Darek", "Kopałko", 2000.0, 'M', 10);
+        Worker worker2 = new Worker("Marek", "Kopałko", 2500.0, 'M', 10);
+        Worker worker3 = new Worker("Marek", "Kopałko", 3000.0, 'M', 10);
+
+        firm.addWorker(worker1);
+        firm.addWorker(worker2);
+        firm.addWorker(worker3);
+
+        firm.saveBinary("firm.bin");
+
+        Workforce firm2 = new Workforce(5);
+//        firm2.openBinary("firm.bin");
+        System.out.println(firm2);
     }
 
 }
