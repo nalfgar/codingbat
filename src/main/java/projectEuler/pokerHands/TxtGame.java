ebdb83a6cbb2f1e2371d9965ef6a0c37dba06036
Darek Strojecki <dstroj@wp.pl>
2018-01-30 13:40:22 +0100

    Add TxtGame - read hands from file and count when Player1 wins.

---
+++ src/main/java/projectEuler/pokerHands/TxtGame.java
@@ -0,0 +1,32 @@
+package projectEuler.pokerHands;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class TxtGame {
+    public static void main(String[] args) {
+        int playerOneWinCounter = 0;
+        Scanner fileScanner = null;
+        Game game;
+        String line;
+        try {
+            fileScanner = new Scanner(new File("p054_poker.txt"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+
+        while (fileScanner.hasNext()) {
+            line = fileScanner.nextLine();
+            game = new Game(line);
+            if (game.whoWins() == 1) {
+                playerOneWinCounter++;
+            }
+        }
+
+        fileScanner.close();
+        System.out.println(playerOneWinCounter);
+    }
+
+}
