ff74ba29665cbaf9f9c07ea439706ee9118fa4d8
Darek Strojecki <dstroj@wp.pl>
2018-02-11 13:27:00 +0100

    Adapted test class to assertJ.

--- src/test/java/warmup_1/abs21/Abs21Test.java
+++ src/test/java/warmup_1/abs21/Abs21Test.java
@@ -2,40 +2,33 @@ package warmup_1.abs21;
 
 import org.junit.Test;
 
-
+import static org.assertj.core.api.Java6Assertions.assertThat;
 import static warmup_1.abs21.Abs21.abs21;
-import static org.junit.Assert.assertEquals;
 
 public class Abs21Test {
 
     @Test
     public void test19(){
-        assertEquals(2, abs21(19));
+        assertThat(abs21(19)).isEqualTo(2);
     }
 
     @Test
     public void test10(){
-        assertEquals(10, abs21(11));
+        assertThat(abs21(11)).isEqualTo(10);
     }
 
     @Test
     public void test21(){
-        assertEquals(0, abs21(21));
+        assertThat(abs21(21)).isEqualTo(0);
     }
 
     @Test
     public void test22(){
-        assertEquals(1*2, abs21(22));
+        assertThat(abs21(22)).isEqualTo(1*2);
     }
 
     @Test
     public void test25(){
-        assertEquals(4*2, abs21(25));
+        assertThat(abs21(25)).isEqualTo(4*2);
     }
-
-
-
-
-
-
 }
