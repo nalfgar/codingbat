c9194eef75cb59f63f8859e0ee32cb286631cf79
Darek Strojecki <dstroj@wp.pl>
2018-02-11 13:30:24 +0100

    Adapted test class to assertJ.

--- src/test/java/warmup_1/backAround/backAroundTest.java
+++ src/test/java/warmup_1/backAround/backAroundTest.java
@@ -2,18 +2,19 @@ package warmup_1.backAround;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.assertj.core.api.Java6Assertions.assertThat;
+import static warmup_1.backAround.BackAround.backAround;
 
 public class backAroundTest {
 
     @Test
     public void oneLetterWord(){
-        assertEquals("aaa", BackAround.backAround("a"));
+        assertThat(backAround("a")).isEqualTo("aaa");
     }
 
     @Test
     public void moreThanOneLetterWord(){
-        assertEquals("tcatt", BackAround.backAround("cat"));
+        assertThat(backAround("cat")).isEqualTo("tcatt");
     }
 
 }
