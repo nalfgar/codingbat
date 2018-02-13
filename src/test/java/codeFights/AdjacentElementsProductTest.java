58e2e8c39477829715a24fba503818e9c03b017b
Darek Strojecki <dstroj@wp.pl>
2018-02-13 19:43:31 +0100

    Add djacentElementsProduct class with tests.

---
+++ src/test/java/codeFights/AdjacentElementsProductTest.java
@@ -0,0 +1,37 @@
+package codeFights;
+
+import org.junit.Test;
+
+import static codeFights.AdjacentElementsProduct.adjacentElementsProduct;
+import static org.assertj.core.api.Java6Assertions.assertThat;
+
+public class AdjacentElementsProductTest {
+
+    @Test
+    public void testOne() {
+        int[] inputArray = new int[]{3, 6, -2, -5, 7, 3};
+
+        assertThat(adjacentElementsProduct(inputArray)).isEqualTo(21);
+    }
+
+    @Test
+    public void testTwo() {
+        int[] inputArray = new int[]{-1, -2};
+
+        assertThat(adjacentElementsProduct(inputArray)).isEqualTo(2);
+    }
+
+    @Test
+    public void testThree() {
+        int[] inputArray = new int[]{5, 1, 2, 3, 1, 4};
+
+        assertThat(adjacentElementsProduct(inputArray)).isEqualTo(6);
+    }
+
+    @Test
+    public void testFour() {
+        int[] inputArray = new int[]{1, 0, 1, 0, 1000};
+
+        assertThat(adjacentElementsProduct(inputArray)).isEqualTo(0);
+    }
+}
