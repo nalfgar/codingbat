6fc8a69a3bda2d74df04d9a116ab644d3560cd39
Darek Strojecki <dstroj@wp.pl>
2018-02-15 16:26:17 +0100

    Add AllLongestStrings class with test.

---
+++ src/main/java/codeFights/AllLongestStrings.java
@@ -0,0 +1,30 @@
+package codeFights;
+
+import java.util.LinkedList;
+import java.util.List;
+
+public class AllLongestStrings {
+    public static String[] allLongestStrings(String[] inputArray) {
+        int maxLenght = 0;
+        List<String> listMax = new LinkedList<>();
+        String[] result;
+
+        for (String s : inputArray) {
+            if (s.length() > maxLenght) {
+                maxLenght = s.length();
+            }
+        }
+
+        for (String s : inputArray) {
+            if (s.length() == maxLenght) {
+                listMax.add(s);
+            }
+        }
+        result = new String[listMax.size()];
+        for (int i = 0; i < listMax.size(); i++) {
+            result[i] = listMax.get(i);
+        }
+
+        return result;
+    }
+}
