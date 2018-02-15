5b3585838df410e1339e9369bb977fe13ffa313c
Darek Strojecki <dstroj@wp.pl>
2018-02-15 17:30:07 +0100

    Add CommonCharacterCount class with test.

---
+++ src/main/java/codeFights/CommonCharacterCount.java
@@ -0,0 +1,40 @@
+package codeFights;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class CommonCharacterCount {
+    public static int commonCharacterCount(String s1, String s2) {
+        int counter = 0;
+        Map<String, Integer> map = new HashMap<>();
+        String letter1;
+
+        fillMap(s2, map);
+
+        return analizeData(s1, counter, map);
+    }
+
+    private static int analizeData(String s1, int counter, Map<String, Integer> map) {
+        String letter1;
+        for (int i = 0; i < s1.length(); i++) {
+            letter1 = String.valueOf(s1.charAt(i));
+            if (map.containsKey(letter1) && map.get(letter1) > 0){
+                counter++;
+                map.put(letter1, map.get(letter1)-1);
+            }
+        }
+        return counter;
+    }
+
+    private static void fillMap(String s2, Map<String, Integer> map) {
+        String letter1;
+        for (int i = 0; i < s2.length(); i++) {
+            letter1 = String.valueOf(s2.charAt(i));
+            if (!map.containsKey(letter1)){
+                map.put(letter1, 1);
+            } else {
+                map.put(letter1, map.get(letter1)+1);
+            }
+        }
+    }
+}
