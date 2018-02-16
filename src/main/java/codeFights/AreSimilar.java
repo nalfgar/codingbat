c70604cd7ca888f1e6fa73b9aac2a34cb6b66a73
Darek Strojecki <dstroj@wp.pl>
2018-02-16 22:51:55 +0100

    Add areSimilar class with tests.

---
+++ src/main/java/codeFights/AreSimilar.java
@@ -0,0 +1,45 @@
+package codeFights;
+
+public class AreSimilar {
+    public static boolean areSimilar(int[] a, int[] b) {
+        int[] arrayDifference;
+        int theSameElementCounter = getNumberOfTheSameElementsInArrays(a, b);
+
+        if (theSameElementCounter == a.length) {
+            return true;
+        } else if (a.length - theSameElementCounter > 2) {
+            return false;
+        }
+        arrayDifference = getDifferences(a, b);
+        if (arrayDifference[0] == arrayDifference[3] && arrayDifference[1] == arrayDifference[2]) {
+            return true;
+        }
+
+        return false;
+    }
+
+    private static int[] getDifferences(int[] a, int[] b) {
+        int[] result = new int[4];
+        int loopCounter = 0;
+
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] != b[i]) {
+                result[loopCounter] = a[i];
+                result[loopCounter + 1] = b[i];
+                loopCounter += 2;
+            }
+        }
+
+        return result;
+    }
+
+    private static int getNumberOfTheSameElementsInArrays(int[] a, int[] b) {
+        int counter = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == b[i]) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
