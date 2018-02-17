ada0154634910296c658479ff515407d65446205
Darek Strojecki <dstroj@wp.pl>
2018-02-17 15:26:02 +0100

    Add AreEquallyStrong class with tests.

---
+++ src/main/java/codeFights/AreEquallyStrong.java
@@ -0,0 +1,22 @@
+package codeFights;
+
+public class AreEquallyStrong {
+    public static boolean areEquallyStrong(int yourLeft, int yourRight, int friendsLeft, int friendsRight) {
+        int youSum = yourLeft + yourRight;
+        int friendSum = friendsLeft + friendsRight;
+
+        if (youSum != friendSum){
+            return false;
+        } else if (youSum == friendSum && pairEqual(yourLeft, yourRight, friendsLeft, friendsRight)){
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean pairEqual(int yourLeft, int yourRight, int friendsLeft, int friendsRight) {
+        if ((yourLeft == friendsLeft && yourRight == friendsRight) || (yourLeft == friendsRight && yourRight == friendsLeft)){
+            return true;
+        }
+        return false;
+    }
+}
