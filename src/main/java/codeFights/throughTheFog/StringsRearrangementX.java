ad68348d485c889c60ebe9fca18eaf2e74c4b396
Darek Strojecki <dstroj@wp.pl>
2018-02-19 20:54:12 +0100

    Add StringsRearrangement class with tests. Class does not work!!!

---
+++ src/main/java/codeFights/throughTheFog/StringsRearrangementX.java
@@ -0,0 +1,38 @@
+package codeFights.throughTheFog;
+
+public class StringsRearrangementX {
+    boolean success = false; // modified by findSequence
+
+
+    static boolean stringsRearrangementX(String[] inputArray) {
+
+//        boolean[] used = new boolean[inputArray.length];
+//        findSequence(inputArray, inputArray[0], used, 0);
+//        return success;
+//    }
+//
+//    static void findSequence(String[] a, String prev, boolean[] used, int n) {
+//        if (n == a.length) {
+//            success = true;
+//            return;
+//        }
+//        for (int i = 0; i < a.length; i++) {
+//            if (!used[i] && (prev == null || differByOne(prev, a[i]))) {
+//                used[i] = true;
+//                findSequence( ?, a[i], used, n + 1);
+//                used[i] = false;
+//            }
+//        }
+//    }
+//
+//    static boolean differByOne(String a, String b) {
+//        int count = 0;
+//        for (int i = 0; i < a.length(); i++) {
+//            if (a.charAt(i) != b.charAt(i)) {
+//                count++;
+//            }
+//        }
+//        return count == ?;
+//    }
+        return false;
+}}
