6ab9cf226822db0bf108baeffb12cde969c19427
Darek Strojecki <dstroj@wp.pl>
2018-02-20 19:48:44 +0100

    Add FirstDuplicate class with test.

---
+++ src/main/java/codeFights/Arrays/FirstDuplicate.java
@@ -0,0 +1,20 @@
+package codeFights.Arrays;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FirstDuplicate {
+    public static int firstDuplicate(int[] array) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i : array) {
+            if (!map.containsKey(i)){
+                map.put(i, 1);
+            } else {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
