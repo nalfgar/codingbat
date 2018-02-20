5ad96a3054731838757fef9d63fc0d74d1918b24
Darek Strojecki <dstroj@wp.pl>
2018-02-20 12:53:36 +0100

    Add KnapsackLight class with tests. Class WORKS properly!!!

--- src/main/java/codeFights/atTheCrossroads/knapsackLight/KnapsackLight.java
+++ src/main/java/codeFights/atTheCrossroads/knapsackLight/KnapsackLight.java
@@ -1,14 +1,34 @@
 package codeFights.atTheCrossroads.knapsackLight;
 
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.Map;
+
 public class KnapsackLight {
     public static int knapsackLight(int value1, int weight1, int value2, int weight2, int maxW) {
+        Map<Integer, Integer> map = new HashMap<>();
+        map.put(weight1, value1);
+        map.put(weight2, value2);
+        int strongerHand = 0;
+        int weakestHand = 0;
 
-        if (weight1 + weight2 <= maxW){
+
+        if (weight1 + weight2 <= maxW) {
             return value1 + value2;
-        } else if (weight1 <= maxW && weight1 > weight2){
-            return value1;
-        } else if (weight2 <= maxW && weight2 > weight1){
-            return value2;
+        } else if (weight1 > maxW && weight2 > maxW) {
+            return 0;
+        } else {
+            strongerHand = map.keySet().stream().max(Comparator.naturalOrder()).get();
+            weakestHand = map.keySet().stream().min(Comparator.naturalOrder()).get();
+            if (strongerHand >= maxW) {
+                return map.get(weakestHand);
+            } else if (map.get(strongerHand) >= map.get(weakestHand)) {
+                return map.get(strongerHand);
+            } else if(map.get(weakestHand) >= map.get(strongerHand)){
+                return map.get(weakestHand);
+            } else {
+                map.get(weakestHand);
+            }
         }
         return 0;
     }
