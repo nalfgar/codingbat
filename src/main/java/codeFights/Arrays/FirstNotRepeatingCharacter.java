b24bbc3f6de1a07657181c3d2d3861edb110094e
Darek Strojecki <dstroj@wp.pl>
2018-02-20 20:11:11 +0100

    Add FirstNotRepeatingCharacter class with test.

---
+++ src/main/java/codeFights/Arrays/FirstNotRepeatingCharacter.java
@@ -0,0 +1,28 @@
+package codeFights.Arrays;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class FirstNotRepeatingCharacter {
+    public static char firstNotRepeatingCharacter(String s) {
+        Map<String, Integer> map = new LinkedHashMap<>();
+        String letter;
+
+        for (int i = 0; i < s.length(); i++) {
+            letter = String.valueOf(s.charAt(i));
+            if (!map.containsKey(letter)) {
+                map.put(letter, 1);
+            } else {
+                map.put(letter, map.get(letter) + 1);
+            }
+        }
+
+        for (Map.Entry<String, Integer> stringIntegerEntry : map.entrySet()) {
+            if (stringIntegerEntry.getValue() == 1) {
+                return stringIntegerEntry.getKey().charAt(0);
+            }
+        }
+        return '_';
+
+    }
+}
