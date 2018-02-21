d672bb0158240029efc352b12b841860168a3eae
Darek Strojecki <dstroj@wp.pl>
2018-02-21 22:44:19 +0100

    Add RotateImage class with tests.

---
+++ src/main/java/codeFights/tools/Tools.java
@@ -0,0 +1,14 @@
+package codeFights.tools;
+
+public class Tools {
+
+    public static void printArray(int[][] result) {
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[0].length; j++) {
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+}
